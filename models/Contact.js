f2f200143cd23548874c007ccbc27f0e17676165
almat-vitalogy <almat@vitalogy.ai>
2025-05-26 05:38:59 +0800

    activity update and send message fix

--- models/Contact.js
+++ models/Contact.js
@@ -3,8 +3,15 @@ const mongoose = require("mongoose");
 const ContactSchema = new mongoose.Schema({
   userEmail: { type: String, required: true, ref: "User", index: true },
   phone: { type: String, required: true, index: true },
-  name: { type: String, default: function() { return this.phone; } },
-  labels: [{ type: String }]
+  name: {
+    type: String,
+    default: function () {
+      return this.phone;
+    },
+  },
+  labels: [{ type: String }],
 });
+
+ContactSchema.index({ userEmail: 1, phone: 1 }, { unique: true });
 
 module.exports = mongoose.model("Contact", ContactSchema);
